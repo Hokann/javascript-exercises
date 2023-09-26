640233dce78d05a58286ae66aad760391ecec290
Hokann <hokan4316v3@gmail.com>
2023-09-26 19:50:57 -0400

    finish js exos 1-7

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = ''
+    for (let i = 0; i< string.length; i++){
+        let lastLetter = string.slice(i,i+1)
+        //positioning each lastLetter of original string to new reversedString.
+        reversedString = [lastLetter, reversedString].join('');
+         
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if (!Number.isInteger(int1) || !Number.isInteger(int2)) return 'ERROR';
+    if (int1<0 || int2<0) return 'ERROR';
+    let sum = 0;
+    for (let i=Math.min(int1,int2); i<=Math.max(int1,int2); i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
